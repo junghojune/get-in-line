7943fe9e87ae1cca7a0a493cfc0e1f860ea0d0f6
정호준 <hojune0904@gmail.com>
2022-01-12 16:57:50 +0900

    Implement #1 - api controller advice
    API 전용 `@RestControllerAdvice` 구현
    스프링 MVC 예외를 처리하기 위해
    `ResponseEntityExceptionHandler` 상속 및
    오버라이딩 코드가 추가된 형태

--- src/main/java/com/hojune/getinline/controller/api/APIEventController.java
+++ src/main/java/com/hojune/getinline/controller/api/APIEventController.java
@@ -1,6 +1,11 @@
 package com.hojune.getinline.controller.api;
 
 
+import com.hojune.getinline.constant.ErrorCode;
+import com.hojune.getinline.dto.APIErrorResponse;
+import com.hojune.getinline.exception.GeneralException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,4 +38,19 @@ public class APIEventController {
     public Boolean removeEvent(@PathVariable Integer eventId){
         return true;
     }
+
+    @ExceptionHandler
+    public ResponseEntity<APIErrorResponse> general(GeneralException e){
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus status = errorCode.isClientSideError() ?
+                HttpStatus.BAD_REQUEST :
+                HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return ResponseEntity
+                .status(status)
+                .body(APIErrorResponse.of(
+                        false, errorCode, errorCode.getMessage(e)
+        ));
+    }
+
 }
