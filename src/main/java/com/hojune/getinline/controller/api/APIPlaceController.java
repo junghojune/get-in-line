49ce80a48eabd8094fc9bd9683e44a4485a0afec
정호준 <hojune0904@gmail.com>
2022-01-22 00:46:42 +0900

    Implement #2 - APIPlaceController, dto, and its tests
    장소 뷰 테스트와 입출력 기초 구현
    역시 임시로 dummy 데이터를 컨트롤러 메소드에 사용
    서비스 구현하면 삭제할 것

--- src/main/java/com/hojune/getinline/controller/api/APIPlaceController.java
+++ src/main/java/com/hojune/getinline/controller/api/APIPlaceController.java
@@ -1,38 +1,65 @@
 package com.hojune.getinline.controller.api;
 
 
+import com.hojune.getinline.constant.PlaceType;
+import com.hojune.getinline.dto.APIDataResponse;
+import com.hojune.getinline.dto.PlaceDTO;
+import com.hojune.getinline.dto.PlaceRequest;
+import com.hojune.getinline.dto.PlaceResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-@RequestMapping("/api")
-@RestController
+
 public class APIPlaceController {
 
     @GetMapping("/places")
-    public List<String> getPlace(){
-        return List.of("place1", "place2");
+    public APIDataResponse<List<PlaceResponse>> getPlaces() {
+        return APIDataResponse.of(List.of(PlaceResponse.of(
+                PlaceType.COMMON,
+                "랄라배드민턴장",
+                "서울시 강남구 강남대로 1234",
+                "010-1234-5678",
+                30,
+                "신장개업"
+        )));
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/places")
-    public boolean creatPlace(){
-        return true;
+    public APIDataResponse<Void> createPlace(@RequestBody PlaceRequest placeRequest) {
+        return APIDataResponse.empty();
     }
 
     @GetMapping("/places/{placeId}")
-    public String getPlace(@PathVariable Integer placeId){
-        return "place" + placeId;
+    public APIDataResponse<PlaceResponse> getPlace(@PathVariable Long placeId) {
+        if (placeId.equals(2L)) {
+            return APIDataResponse.empty();
+        }
+
+        return APIDataResponse.of(PlaceResponse.of(
+                PlaceType.COMMON,
+                "랄라배드민턴장",
+                "서울시 강남구 강남대로 1234",
+                "010-1234-5678",
+                30,
+                "신장개업"
+        ));
     }
 
     @PutMapping("/places/{placeId}")
-    public Boolean modifyPlace(@PathVariable Integer placeId){
-        return true;
+    public APIDataResponse<Void> modifyPlace(
+            @PathVariable Long placeId,
+            @RequestBody PlaceRequest placeRequest
+    ) {
+        return APIDataResponse.empty();
     }
 
     @DeleteMapping("/places/{placeId}")
-    public Boolean removePlace(@PathVariable Integer placeId){
-        return true;
+    public APIDataResponse<Void> removePlace(@PathVariable Long placeId) {
+        return APIDataResponse.empty();
     }
-
 
 }
--- src/test/java/com/hojune/getinline/controller/api/APIAuthControllerTest.java
+++ src/test/java/com/hojune/getinline/controller/api/APIAuthControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
