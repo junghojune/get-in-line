ce8f456c6243df8f713cd488289c9003ab771fc5
정호준 <hojune0904@gmail.com>
2022-01-22 00:51:40 +0900

    Update #1 -  additional files
    파일들중 안쓰는 import들 없애고 들여쓰기등 배치 정렬해준것들

--- src/test/java/com/hojune/getinline/controller/BaseControllerTest.java
+++ src/test/java/com/hojune/getinline/controller/BaseControllerTest.java
@@ -3,9 +3,7 @@ package com.hojune.getinline.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
