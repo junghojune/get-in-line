0292a5e005a5e31caa3987ad00853142ce630c1b
정호준 <hojune0904@gmail.com>
2022-02-15 00:29:01 +0900

    Add #2 - APIEcceptionHandlerTest
    테스트를 작성하고, 이에 맞춰 약간 리팩토링도 진행

--- src/main/java/com/hojune/getinline/controller/error/APIExceptionHandler.java
+++ src/main/java/com/hojune/getinline/controller/error/APIExceptionHandler.java
@@ -19,14 +19,14 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
-        return callSuperInternalExceptionHandler(e, ErrorCode.VALIDATION_ERROR, HttpHeaders.EMPTY, HttpStatus.BAD_REQUEST, request);
+        return handleExceptionInternal(e, ErrorCode.VALIDATION_ERROR, HttpHeaders.EMPTY, HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler
     public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
         ErrorCode errorCode = e.getErrorCode();
         HttpStatus status = errorCode.isClientSideError() ? HttpStatus.BAD_REQUEST : HttpStatus.INTERNAL_SERVER_ERROR;
-        return callSuperInternalExceptionHandler(e, errorCode, HttpHeaders.EMPTY, status, request);
+        return handleExceptionInternal(e, errorCode, HttpHeaders.EMPTY, status, request);
 
     }
 
@@ -35,7 +35,7 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return callSuperInternalExceptionHandler(e, errorCode, HttpHeaders.EMPTY, status, request);
+        return handleExceptionInternal(e, errorCode, HttpHeaders.EMPTY, status, request);
     }
 
     @Override
@@ -44,14 +44,14 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
                 ErrorCode.SPRING_BAD_REQUEST :
                 ErrorCode.SPRING_INTERNAL_ERROR;
 
-        return callSuperInternalExceptionHandler(ex, errorCode, headers, status, request);
+        return handleExceptionInternal(ex, errorCode, headers, status, request);
     }
 
-    private ResponseEntity<Object> callSuperInternalExceptionHandler(Exception ex, ErrorCode errorCode, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    private ResponseEntity<Object> handleExceptionInternal(Exception e, ErrorCode errorCode, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         return super.handleExceptionInternal(
-                ex,
-                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(ex)),
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
                 headers,
                 status,
                 request
