ce2cf73dceedf0cafd1bbb97bfb9fa5edec9b4a6
정호준 <hojune0904@gmail.com>
2022-02-14 16:20:02 +0900

    Add #2 - EventRepository config
    기존의 `EventRepository`를 테스트 빈 등록하는 방식은
    앱을 실행할 수 없게 만들기 때문에
    앱을 실행할 수 있게끔 직접 설정하고 테스트 빈은 삭제
    인터페이스 구현 일부도 수정
    
    하지만 나중에는 이 빈 설정 자체가 필요 없어야 한다

--- src/test/java/com/hojune/getinline/GetInLineApplicationTests.java
+++ src/test/java/com/hojune/getinline/GetInLineApplicationTests.java
@@ -1,27 +1,12 @@
 package com.hojune.getinline;
 
-import com.hojune.getinline.repository.EventRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 class GetInLineApplicationTests {
-
-
     @Test
     void contextLoads() {
 
     }
-
-    //TODO: 불필요 해지면 지울 것
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        EventRepository eventRepository() {
-            return new EventRepository() {};
-        }
-    }
-
 }
