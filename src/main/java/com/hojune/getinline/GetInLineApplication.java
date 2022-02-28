a7123a99b8c1c146c81ffa20282b7ffe81803b45
정호준 <hojune0904@gmail.com>
2022-02-28 17:15:16 +0900

    Add  thymeleaf 3 config, properties for decoupled logic feature
    타임리프 3 기능 중 decoupled logic 을 사용하기 위해
    설정과 프로퍼티 작성

--- src/main/java/com/hojune/getinline/GetInLineApplication.java
+++ src/main/java/com/hojune/getinline/GetInLineApplication.java
@@ -2,7 +2,10 @@ package com.hojune.getinline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class GetInLineApplication {
 
